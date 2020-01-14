85ad29f8147c98199509417b874905f850cf462f
cowhitewhite <252735727@qq.com>
2020-01-14 15:09:48 +0800

    去掉注释

--- src/main/java/com/chen/elegant/enums/UserTypeEnum.java
+++ src/main/java/com/chen/elegant/enums/UserTypeEnum.java
@@ -3,13 +3,6 @@ package com.chen.elegant.enums;
 
 import org.springframework.util.StringUtils;
 
-/**
- * @author zjjlive)
- * @version 1.0
- * @website https://www.foreknow.me
- * @date 2018/4/16 16:26
- * @since 1.0
- */
 public enum UserTypeEnum {
     ROOT("超级管理员"),
     ADMIN("管理员"),
